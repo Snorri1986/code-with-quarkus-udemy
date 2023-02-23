6b48b18881436d8d9461b37158af23811f1749a2
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-02-23 23:20:11 +0100

    #2 after adding quarkus-hibernate-orm-panache Should be fixed shortly

---
+++ src/main/java/org/snorri1986/hibernate/Fruit.java
@@ -0,0 +1,14 @@
+package org.snorri1986.hibernate;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Cacheable
+public class Fruit extends PanacheEntity {
+    @Column(length = 40,unique=true)
+    public String name;
+}
